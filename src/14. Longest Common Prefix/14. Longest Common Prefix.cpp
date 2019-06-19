58f93e57466cf685fa4bf0f1786b7ba2e563148a
ssp1ash <guoyi3299@live.com>
2019-06-19 20:51:17 +0800

    14

---
+++ src/14. Longest Common Prefix/14. Longest Common Prefix.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+    	string result="";
+    	int j=0;
+    	int i=0;
+        while(j<strs[i].size()){
+        	for(i=0;i<strs.size()-1;i++){
+        		if(strs[i][j]==strs[i+1][j]){
+        			continue;
+				}else{
+					return result;
+				}
+			}
+			result+=strs[i][j];
+			j++;
+		}
+		return result;
+    }
+};
+int main(){
+	string a="123";
+	string b="";
+	vector<string> x;
+//	x.push_back(a);
+//	x.push_back(b);
+	Solution q;
+	cout<<q.longestCommonPrefix(x);
+}
