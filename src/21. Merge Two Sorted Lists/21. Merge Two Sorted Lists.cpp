2dacbe90bd4ed3f9ff36e7d7b60b9f37e83284dd
ssp1ash <guoyi3299@live.com>
2019-06-26 17:24:50 +0800

    21recursion

--- src/21. Merge Two Sorted Lists/21. Merge Two Sorted Lists.cpp
+++ src/21. Merge Two Sorted Lists/21. Merge Two Sorted Lists.cpp
@@ -29,6 +29,22 @@ public:
 		}
 		return result.next;
     }
+    
+    ListNode* mergeTwoListsRecursion(ListNode* l1, ListNode* l2) {
+    	if(l1==NULL){
+    		return l2;
+		}
+		if(l2==NULL){
+			return l1;
+		}
+		if(l1->val<l2->val){
+			l1->next=mergeTwoListsRecursion(l1->next,l2);
+			return l1;
+		}else{
+			l2->next=mergeTwoListsRecursion(l2->next,l1);
+			return l2;
+		}
+    }
 };
 int main(){
 	ListNode a(1);
@@ -43,7 +59,8 @@ int main(){
 	x.next=&y;
 	y.next=&z;
 	Solution m;
-	ListNode *result=m.mergeTwoLists(&a,&x);
+//	ListNode *result=m.mergeTwoLists(&a,&x);
+	ListNode *result=m.mergeTwoListsRecursion(&a,&x);
 	while(result!=NULL){
 		cout<<result->val;
 		result=result->next;
